cae08928bdbf4e2a8485db99fdf85793310d2cec
Austin Cooley <adcooley@gmail.com>
2023-01-10 08:52:45 -0600

    initial work

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { gql, useMutation } from '@apollo/client';
-import toast, { Toaster } from 'react-hot-toast';
-import { getSession } from '@auth0/nextjs-auth0';
-import prisma from '../lib/prisma';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { gql, useMutation } from "@apollo/client";
+import toast, { Toaster } from "react-hot-toast";
+import { getSession } from "@auth0/nextjs-auth0";
+import prisma from "../lib/prisma";
 
 const CreateLinkMutation = gql`
   mutation (
@@ -38,14 +38,40 @@ const Admin = () => {
     formState: { errors },
   } = useForm();
 
+  // Upload photo function
+  const uploadPhoto = async (e) => {
+    const file = e.target.files[0];
+    const filename = encodeURIComponent(file.name);
+    const res = await fetch(`/api/upload-image?file=${filename}`);
+    const data = await res.json();
+    const formData = new FormData();
+
+    // @ts-ignore
+    Object.entries({ ...data.fields, file }).forEach(([key, value]) => {
+      formData.append(key, value);
+    });
+
+    toast.promise(
+      fetch(data.url, {
+        method: "POST",
+        body: formData,
+      }),
+      {
+        loading: "Uploading...",
+        success: "Image successfully uploaded!🎉",
+        error: `Upload failed 😥 Please try again ${error}`,
+      }
+    );
+  };
+
   const onSubmit = async (data) => {
     const { title, url, category, description, image } = data;
     const imageUrl = `https://${process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME}.s3.amazonaws.com/${image[0].name}`;
     const variables = { title, url, category, description, imageUrl };
     try {
       toast.promise(createLink({ variables }), {
-        loading: 'Creating new link..',
-        success: 'Link successfully created!🎉',
+        loading: "Creating new link..",
+        success: "Link successfully created!🎉",
         error: `Something went wrong 😥 Please try again -  ${error}`,
       });
     } catch (error) {
@@ -67,7 +93,7 @@ const Admin = () => {
             placeholder="Title"
             name="title"
             type="text"
-            {...register('title', { required: true })}
+            {...register("title", { required: true })}
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </label>
@@ -75,7 +101,7 @@ const Admin = () => {
           <span className="text-gray-700">Description</span>
           <input
             placeholder="Description"
-            {...register('description', { required: true })}
+            {...register("description", { required: true })}
             name="description"
             type="text"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -85,7 +111,7 @@ const Admin = () => {
           <span className="text-gray-700">Url</span>
           <input
             placeholder="https://example.com"
-            {...register('url', { required: true })}
+            {...register("url", { required: true })}
             name="url"
             type="text"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
@@ -95,13 +121,25 @@ const Admin = () => {
           <span className="text-gray-700">Category</span>
           <input
             placeholder="Name"
-            {...register('category', { required: true })}
+            {...register("category", { required: true })}
             name="category"
             type="text"
             className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
           />
         </label>
 
+        <label className="block">
+          <span className="text-gray-700">
+            Upload a .png or .jpg image (max 1MB).
+          </span>
+          <input
+            {...register("image", { required: true })}
+            onChange={uploadPhoto}
+            type="file"
+            accept="image/png, image/jpeg"
+            name="image"
+          />
+        </label>
         <button
           disabled={loading}
           type="submit"
@@ -137,7 +175,7 @@ export const getServerSideProps = async ({ req, res }) => {
     return {
       redirect: {
         permanent: false,
-        destination: '/api/auth/login',
+        destination: "/api/auth/login",
       },
       props: {},
     };
@@ -153,11 +191,11 @@ export const getServerSideProps = async ({ req, res }) => {
     },
   });
 
-  if (user.role !== 'ADMIN') {
+  if (user.role !== "ADMIN") {
     return {
       redirect: {
         permanent: false,
-        destination: '/404',
+        destination: "/404",
       },
       props: {},
     };
